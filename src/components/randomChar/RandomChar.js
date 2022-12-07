4e793c93729a9a4ff438688bf619c4ea13f61112
Vladislav Murzin <vlad99samara@mail.ru>
2022-12-07 21:39:02 +0400

    Live circle hooks + Error Boundaries

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -28,7 +28,14 @@ class RandomChar extends Component {
     onCharLoaded = (char) => {
         this.setState({
             char, 
-            loading: false});
+            loading: false
+        });
+    }
+
+    onCharLoading = () => {
+        this.setState ({
+            loading: true
+        })
     }
 
     onError = () => {
@@ -40,6 +47,7 @@ class RandomChar extends Component {
 
     updateChar = () => {
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+        this.onCharLoading();
         this.marvelService
             .getCharacter(id)
             .then(this.onCharLoaded)
@@ -65,7 +73,7 @@ class RandomChar extends Component {
                     <p className="randomchar__title">
                         Or choose another one
                     </p>
-                    <button className="button button__main">
+                    <button onClick={this.updateChar} className="button button__main">
                         <div className="inner">try it</div>
                     </button>
                     <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
@@ -77,10 +85,13 @@ class RandomChar extends Component {
 
 const View = ({char}) => {
     const {name, description, thumbnail, homepage, wiki} = char;
-
+    let imgStyle = {'objectFit': 'cover'}
+    if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+        imgStyle = {'objectFit': 'contain'};
+    }
     return (
         <div className="randomchar__block">
-        <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+        <img src={thumbnail} alt="Random character" className="randomchar__img" style={imgStyle}/>
         <div className="randomchar__info">
             <p className="randomchar__name">{name}</p>
             <p className="randomchar__descr">
